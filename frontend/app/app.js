a1ce72a3f97de0057603f0c7ce459da015e9d438
FengD <dingbobby@hotmail.com>
2016-12-30 15:47:35 +0100

    delete some files

--- frontend/app/app.js
+++
@@ -1,8 +0,0 @@
-// This is a super simple Hello World AngularJS App
-(function() {
-  angular
-    .module('geolocApp', [])
-    .controller('HomeController', ['$scope', function($scope) {         
-      $scope.hello = 'Hello World';
-    }]);
-})();
