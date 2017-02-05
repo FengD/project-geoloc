b7a7b6f6b55c2ee6cf58fbb34ae9cd8c8f0f97c7
FengD <dingbobby@hotmail.com>
2017-02-05 20:13:58 +0100

    chart with position data

--- backend/chart/chartServer.js
+++ backend/chart/chartServer.js
@@ -44,7 +44,7 @@ var initChartServer = function(questionNB){
 		logger.info("Connected");
 		
 		// when the client emits 'adduser', this listens and executes
-		socket.on('adduser', function(username, questionStep){
+		socket.on('adduser', function(username, questionStep, position, date){
 			// store the username in the socket session for this client
 			socket.username = username;
 			// store the room name in the socket session for this client
@@ -56,20 +56,20 @@ var initChartServer = function(questionNB){
 			// echo to client they've connected
 			// socket.emit('updatechat', 'SERVER', 'you have connected to room'+questionStep);
 			// echo to room 1 that a person has connected to their room
-			socket.broadcast.to('question'+questionStep).emit('updatechat', 'ADMIN', username + ' is now answering this question.');
+			socket.broadcast.to('question'+questionStep).emit('updatechat', 'ADMIN', username + ' is now answering this question. He is at (' + position + ")  "+date);
 			socket.emit('updaterooms', chartGroups, 'question'+questionStep);
 		});
 		
 		// when the client emits 'sendchat', this listens and executes
-		socket.on('sendchat', function (data) {
+		socket.on('sendchat', function (data,position,date) {
 			// we tell the client to execute 'updatechat' with 2 parameters
-			io.sockets.in(socket.question).emit('updatechat', socket.username, data);
+			io.sockets.in(socket.question).emit('updatechat', socket.username, data,position, date);
 		});
 		
-		socket.on('switchRoom', function(newQuestion){
+		socket.on('switchRoom', function(newQuestion, position,date){
 			socket.leave(socket.question);
 			socket.join(newQuestion);
-			socket.emit('updatechat', 'ADMIN', 'you are now answering '+ newQuestion);
+			socket.emit('updatechat', 'ADMIN', 'you are now answering '+ newQuestion+"He is at (" + position +  ")  "+date);
 			// sent message to OLD room
 			socket.broadcast.to(socket.question).emit('updatechat', 'ADMIN', socket.username+' passed this question correctly.');
 			// update socket session room title
--- frontend/app/components/header/headerController.js
+++ frontend/app/components/header/headerController.js
@@ -25,11 +25,22 @@ angular.module('geolocApp')
             $rootScope.userType = $cookies.get('userType');
             $rootScope.question_step = $cookies.get('question_step');
             $rootScope.userPhoto = $cookies.get('photo_path');
+            
             socket.on('connect', function(){
+                var lat = 0,long = 0;
+                var date = new Date();
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function (position) {
+                        lat = position.coords.latitude;
+                        long = position.coords.longitude;
+                        socket.emit('adduser', $cookies.get('name'), $cookies.get('question_step'),"("+lat + "," + long + ")",date);
+                    });      
+                }
                 // call the server-side function 'adduser' and send one parameter (value of prompt)
-                socket.emit('adduser', $cookies.get('name'), $cookies.get('question_step'));
-                socket.on('updatechat', function (username, data) {
-                    console.log(username + ":" + data);
+               
+                socket.on('updatechat', function (username, data, position, date) {
+                    console.log(username + ":" + data + "position:" + position + "date:" + date);
+                    $scope.allChart.push(username + ":" + data + "position:" + position + "date:" + date);
                 });
             });
         }
@@ -66,18 +77,58 @@ angular.module('geolocApp')
           $scope.showAdvanced = function(ev) {
             // Appending dialog to document.body to cover sidenav in docs app
             var confirm = $mdDialog.prompt()
-              .title('Chart')
-              .textContent('What do you want to say?')
-              .placeholder('text')
-              .ariaLabel('Dog name')
+              // .title('Chart')
+              // .textContent('What do you want to say?')
+              // .textContent('What do you want to say?')
+              // .textContent('What do you want to say?')
+              // .textContent('What do you want to say?')
+              .placeholder('Send message to your frinds.')
+              // .ariaLabel('Dog name')
               .targetEvent(ev)
               .ok('SEND')
               .cancel('CANCEL');
 
-            $mdDialog.show(confirm).then(function(result) {
-              socket.emit('sendchat', result);
+            
+
+            $mdDialog.show(
+                {
+                    parent: angular.element(document.body),
+                    clickOutsideToClose:true,
+                    template:
+                    '<md-dialog md-theme="mytheme">' +
+                    '  <md-dialog-content>'+
+                         "<div><div><p><span>2</span></p><ul class=\"chartMessage\"><li ng-repeat='chart in allChart'><p>{{chart}}</p></li></ul></div><h4>Leave a Comment:</h4><form><div><textarea></textarea></div><button >Submit</button></form></div>"+
+                    '        <md-button ng-click="closeDialog();">CANCEL</md-button>'+
+                    '        <md-button ng-click=";">SEND</md-button>'+
+                      '  </md-dialog-content>' +
+                      '</md-dialog>',
+                    locals: {
+                    
+                    },
+                    controller: DialogController
+                  }
+
+                ).then(function(result) {
+                var lat = 0,long = 0;
+                var date = new Date();
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function (position) {
+                        lat = position.coords.latitude;
+                        long = position.coords.longitude;
+                        socket.emit('sendchat', result,"("+lat + "," + long + ")",date);
+                    });      
+                }
+                
             }, function() {
+                
             });
-          };
+
+            function DialogController($scope, $mdDialog) {
+                $scope.closeDialog = function() {
+                  $mdDialog.hide();
+                };
+            };
+
+        };
 
     });
